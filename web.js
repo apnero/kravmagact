fbf1e7383ffeb39d12d62cafed3aa816a6f48f40
Andrew Nero <Andrew.nero@gmail.com>
2013-09-01 04:40:46 +0000

    email and gallery

--- web.js
+++ web.js
@@ -2,6 +2,23 @@ var express = require('express');
 var app = express.createServer();
 app.use(express.static(__dirname ));
 
+var postmark = require("postmark")(process.env.POSTMARK_API_KEY)
+
+postmark.send({
+    "From": "apnero@gmail.com",
+    "To": "andrew.nero@gmail.com",
+    "Subject": "Hello from Postmark",
+    "TextBody": "Hello!",
+    "Tag": "big-bang"
+}, function(error, success) {
+    if(error) {
+        console.error("Unable to send via postmark: " + error.message);
+       return;
+    }
+    console.info("Sent to postmark for delivery")
+});
+
+
 var port = process.env.PORT || 8080;
 
 app.listen(port);
