c228c5771641cee73f563c69ac07421e9e809389
Andrew Nero <Andrew.nero@gmail.com>
2013-09-04 05:39:47 +0000

    working with chris

---
+++ assets/js/pages/footer.js
@@ -0,0 +1,52 @@
+document.write(
+"	<div class='footer'>" +
+"		<div class='container'>" +
+"		<div class='row-fluid'>" +
+"		<div class='span4'>" +
+"           <div class='headline'><h3>Contact Us</h3></div>" +
+"		<form>" +
+"           <label>Name</label>" +
+"                <input type='text' class='span7 border-radius-none' />" +
+"                <label>Email<span class='color-red'>*</span></label>" +
+"                <input type='text' class='span border-radius-none' />" +
+"                <label>Phone Number</label>" +
+"                <input type='text' class='span border-radius-none' />" +
+"				<input type='checkbox' />" +
+"				I am interested in a free trial." +
+"                <label>Message</label>" +
+"                <textarea rows='4' class='span12'></textarea>" +
+"                <p><button type='submit' class='btn-u'>Send Message</button></p>" +
+"            </form>" +
+"        </div><!--/span4-->" +			
+"			<div class='span4'>" +
+"                <div class='posts'>" +
+"                    <div class='headline'><h3>Latest News</h3></div>" +
+"                    <dl class='dl-horizontal'>" +
+"                        <dt><a href='#'><img src='assets/img/sliders/elastislide/6.jpg' alt='' /></a></dt>" +
+"                        <dd>" +
+"                            <p><a href='#'>Ladies Night in September.  Bring a friend.</a></p>" +
+"                        </dd>" +
+"                    </dl>" +
+"                    <dl class='dl-horizontal'>" +
+"                    <dt><a href='#'><img src='assets/img/sliders/elastislide/10.jpg' alt='' /></a></dt>" +
+"                        <dd>" +
+"                            <p><a href='#'>We are painting the studio in September.</a></p>" +
+"                        </dd>" +
+"                    </dl>" +
+"                    <dl class='dl-horizontal'>" +
+"                    <dt><a href='#'><img src='assets/img/sliders/elastislide/11.jpg' alt='' /></a></dt>" +
+"                        <dd>" +
+"                            <p><a href='#'>Anim moon officiamplate</a></p>" + 
+"                        </dd>" +
+"                    </dl>" +
+"                </div>" +
+"			</div><!--/span4-->" +
+"	        <!-- Blog Latest Tweets -->" +
+"            <div class='blog-twitter span4'>" +
+"            	<a class='twitter-timeline'  href='https://twitter.com/CannonRidgeTC'  data-widget-id='375080039445823488'>Tweets by @CannonRidgeTC</a>" +
+"		</div>" +
+"		</div><!--/row-fluid-->	" +
+"	</div><!--/container-->	" +
+"</div><!--/footer-->"
+);
+<!--=== End Footer ===-->
---
+++ assets/js/pages/header.js
@@ -0,0 +1,52 @@
+document.write(
+"<div class='header'>" +              
+"    <div class='container'>" + 
+"        <!-- Logo -->" +       
+"        <div class='logo'>" +                                             
+"            <a href='index.html'><img id='logo-header' src='assets/img/logo_9_1_green.001.jpg' alt='Logo'></a>" +
+"        </div><!-- /logo -->" +                                           
+"        <!-- Menu -->" +
+"        <div class='navbar'>" +                                
+"            <div class='navbar-inner'>" +                                  
+"                <a class='btn btn-navbar' data-toggle='collapse' data-target='.nav-collapse'>" +
+"                    <span class='icon-bar'></span>" +
+"                    <span class='icon-bar'></span>" +
+"                    <span class='icon-bar'></span>" +
+"                </a><!-- /nav-collapse -->" +                                  
+"                <div class='nav-collapse collapse'>" +                                     
+"                    <ul class='nav top-2'>" +
+"					    <li>" +
+"                            <a href='index.html'>Home</a>" +                
+"                        </li>" +
+"                        <li>" +
+"                            <a href='page_about.html'>About Us</a>" +                
+"                        </li>" +
+"                        <li>" +
+"                            <a href='#' class='dropdown-toggle' data-toggle='dropdown'>Classes" +
+"                                <b class='caret'></b>" +                           
+"                            </a>" +
+"                            <ul class='dropdown-menu'>" +
+"                                <li><a href='page_defence.html'>Self Defense</a></li>" +
+"                                <li><a href='page_fitness.html'>Fitness</a></li>" +
+"                                <li><a href='page_intelligence.html'>Intelligence</a></li>" +
+"                            </ul>" +
+"                            <b class='caret-out'></b>" +                        
+"                        </li>" +
+"                        <li>" +
+"                            <a href='page_schedule.html'>Schedule</a>" +                
+"                        </li>" +
+"                        <li>" +
+"                            <a href='page_gallery.html'>Gallery</a>" +                
+"                        </li>" +
+"                        <li>" +
+"                            <a href='page_pricing.html'>Pricing</a>" +                
+"                        </li>" +
+"                        <li>" +
+"                            <a href='page_contact.html'>Contact</a>" +                
+"                        </li>" +
+"                </div><!-- /nav-collapse -->" +                                
+"            </div><!-- /navbar-inner -->" +
+"        </div><!-- /navbar -->" +                          
+"    </div><!-- /container -->" +               
+"</div>"
+);
